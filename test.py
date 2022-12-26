bd8556221244ca248c4a05af3907e11bacda9338
LYQ1021 <119857971+LYQ1021@users.noreply.github.com>
2022-12-26 16:34:54 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,20 +5,20 @@ year_list={'2022','2021','2020','2019','2018'}
 option_year=st.sidebar.selectbox("選擇年度",year_list)
 
 if option_year=='2022':
-           type_list={'抒情歌曲':,
-                      '流行歌曲':}
-if option_year=='2021':
-           type_list={'抒情歌曲':,
-                      '流行歌曲':}
-if option_year=='2020':
-           type_list={'抒情歌曲':,
-                      '流行歌曲':}
-if option_year=='2019':
-           type_list={'抒情歌曲':,
-                      '流行歌曲':}
-if option_year=='2018':
-           type_list={'抒情歌曲':,
-                      '流行歌曲':}
+           type_list={'抒情歌曲':{'2018流行1告白氣球'},
+                      '流行歌曲':{}}
+#if option_year=='2021':
+#           type_list={'抒情歌曲':,
+#                      '流行歌曲':}
+#if option_year=='2020':
+#           type_list={'抒情歌曲':,
+#                      '流行歌曲':}
+#if option_year=='2019':
+#           type_list={'抒情歌曲':,
+#                      '流行歌曲':}
+#if option_year=='2018':
+#           type_list={'抒情歌曲':,
+#                      '流行歌曲':}
 option_musiclist=st.sidebar.selectbox("選擇類型",type_list)  
 option_music=st.selectbox("選擇音樂",type_list[option_musiclist])
 
