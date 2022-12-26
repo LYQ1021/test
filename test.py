3b348dae023494add07e7f31351ad0c7afbaa7a6
LYQ1021 <119857971+LYQ1021@users.noreply.github.com>
2022-12-26 17:28:09 +0800

    Update test.py

--- test.py
+++ test.py
@@ -13,12 +13,13 @@ option_year=st.sidebar.selectbox("選擇年度",year_list)
 #if option_year=='2020':
 #           type_list={'抒情歌曲':,
 #                      '流行歌曲':}
-#if option_year=='2019':
-#           type_list={'抒情歌曲':{'小幸運','光年之外','漂向北方','雨蝶'},
-#                      '流行歌曲':{}
+if option_year=='2019':
+           type_list={'抒情歌曲':{'小幸運','光年之外','漂向北方','雨蝶'},
+                      '流行歌曲':{'那些你很冒險的夢','浪子回頭','浪流連','長途夜車','年少有為'}
 if option_year=='2018':
            type_list={'抒情歌曲':{'那些年','愛你','淋雨一直走','安靜','暖暖'},
                       '流行歌曲':{'告白氣球','是什麼讓我遇見這樣的你','寂寞寂寞就好','還是要幸福','等一個人'}
+                      
 option_musiclist=st.sidebar.selectbox("選擇類型",type_list)  
 option_music=st.selectbox("選擇音樂",type_list[option_musiclist])
 
